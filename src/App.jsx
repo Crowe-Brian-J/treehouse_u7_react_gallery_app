15063e988a30bdd45e8519b65fd772c19c093b83
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-20 21:56:40 -0400

    component-ize Search.jsx, stub App.jsx to fetchData, remove search form from index.html

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,14 @@ import PhotoList from './components/PhotoList'
 const App = () => {
   const [count, setCount] = useState(0)
 
+  // So the search bar doesn't break for now
+  const fetchData = (query) => {
+    console.log('Fetching Data for: ', query)
+  }
+
   return (
     <div className="container">
-      <Search />
+      <Search fetchData={fetchData} />
       <Nav />
       <PhotoList />
     </div>
