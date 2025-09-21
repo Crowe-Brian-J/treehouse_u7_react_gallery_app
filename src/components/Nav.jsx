fe223c6b9ae4d89372c05a2394a39040d3deb2f2
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-20 21:50:15 -0400

    import BrowserRouter to main.jsx, remove nav from index.html, implement Nav.jsx in App.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,24 @@
+import { NavLink } from 'react-router-dom'
+
 const Nav = () => {
-  return <nav className="main-nav">{/* nav links will go here */}</nav>
+  const topics = ['cats', 'dogs', 'computers']
+
+  return (
+    <nav className="main-nav">
+      <ul>
+        {topics.map((topic) => (
+          <li key={topic}>
+            <NavLink
+              to={`/${topic}`}
+              className={({ isActive }) => (isActive ? 'active' : '')}
+            >
+              {topic.charAt(0).toUpperCase() + topic.slice(1)}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
 }
 
 export default Nav
